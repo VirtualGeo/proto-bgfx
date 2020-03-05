e322be0562c5d4cdcd71ab3a76e956c280f6615b
gauthier bouyjou <gauthier.bouyjou@diginext.local>
2020-03-05 15:34:21 +0100

    bgfx.cmake helloworld example done

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #include "GLFW/glfw3.h"
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include "GLFW/glfw3native.h"
+#include <bx/uint32_t.h>
+#include "logo.h"
 
 #include <iostream>
 #include <chrono>
 #include <thread>
 
-#define WIN_WIDTH 1600
-#define WIN_HEIGHT 900
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
 
 void init();
 void shutdown();
@@ -55,6 +57,18 @@ void update()
     bgfx::setViewRect(0, 0, 0, uint16_t(WIN_WIDTH), uint16_t(WIN_HEIGHT));
     bgfx::touch(0);
 
+    bgfx::dbgTextClear();
+    bgfx::dbgTextImage(
+        bx::max<uint16_t>(uint16_t(WIN_WIDTH / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(WIN_HEIGHT / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
+
+    bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+
+    bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+    bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+
+    const bgfx::Stats *stats = bgfx::getStats();
+    bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
+
     bgfx::frame();
 }
 
@@ -65,11 +79,12 @@ void init()
     window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello, bgfx from glfw!", NULL, NULL);
     if (window == NULL)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        // std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        exit(-1);
+        throw std::runtime_error("Failed to create GLFW window");
+        // exit(-1);
     }
-    // glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window); // question : what does this fonction ?
     // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     bgfx::PlatformData pd;
@@ -77,6 +92,7 @@ void init()
     bgfx::setPlatformData(pd);
 
     bgfx::Init bgfxInit;
+    // bgfxInit.type = bgfx::RendererType::OpenGL;
     bgfxInit.type = bgfx::RendererType::Count;
     bgfxInit.resolution.width = WIN_WIDTH;
     bgfxInit.resolution.height = WIN_HEIGHT;
@@ -88,13 +104,19 @@ void init()
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xFF0000FF, 1.0f, 0);
     bgfx::setViewRect(0, 0, 0, WIN_WIDTH, WIN_HEIGHT);
     bgfx::touch(0);
+
+    glfwMakeContextCurrent(nullptr); // question : why we can do it ?
 }
 
 void shutdown()
 {
-    glfwTerminate();
-    window = nullptr;
+    glfwMakeContextCurrent(window);
     bgfx::shutdown();
+
+    glfwMakeContextCurrent(nullptr);
+    glfwDestroyWindow(window);
+    // window = nullptr; // question : necessary ?
+    glfwTerminate();
 }
 
 void processInput(GLFWwindow *window)
