223c96dcb3a5f93f30166292c737fb5859c6d341
gauthier bouyjou <gauthier.bouyjou@diginext.local>
2020-03-05 11:41:20 +0100

    bgfx + glfw worked, only set background

--- main.cpp
+++ main.cpp
@@ -1,97 +1,52 @@
 
 
-// #include <bgfx/bgfx.h>
-// #include <bx/bx.h>
-
-
-
-// #include <iostream>
-// int main(int argc, const char * argv[]) {
-//     std::cout << "test :)" << '\n'; 
-
-//     // bgfx::init();
-
-//     return 0;
-// }
-
-// #include "glad.h"
-#include <GLFW/glfw3.h>
+#include "bgfx/bgfx.h"
+#include "bgfx/platform.h"
+#include "GLFW/glfw3.h"
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include "GLFW/glfw3native.h"
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+#define WIN_WIDTH 1600
+#define WIN_HEIGHT 900
 
-int main()
+int main(int argc, char const *argv[])
 {
-    // glfw: initialize and configure
-    // ------------------------------
+    std::cout << "hello" << std::endl;
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello, bgfx from glfw!", NULL, NULL);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
+    bgfx::PlatformData pd;
+    pd.nwh = glfwGetWin32Window(window);
+    bgfx::setPlatformData(pd);
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    bgfx::Init bgfxInit;
+    bgfxInit.type = bgfx::RendererType::Count;
+    bgfxInit.resolution.width = WIN_WIDTH;
+    bgfxInit.resolution.height = WIN_HEIGHT;
+    bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx::init(bgfxInit);
+
+    bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xFF0000FF, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, WIN_WIDTH, WIN_HEIGHT);
+    bgfx::touch(0);
+
+    unsigned int counter = 0;
+    while (true)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+        bgfx::frame();
+        counter++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    // {
-    //     std::cout << "Failed to initialize GLAD" << std::endl;
-    //     return -1;
-    // }    
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
