347eb5156f3eae1b8253423cc69d4ca07434b845
gauthier bouyjou <gauthier.bouyjou@diginext.local>
2020-03-05 14:33:18 +0100

    minimal glfw and bgfx example done

--- main.cpp
+++ main.cpp
@@ -10,15 +10,67 @@
 #include <chrono>
 #include <thread>
 
-
 #define WIN_WIDTH 1600
 #define WIN_HEIGHT 900
 
+void init();
+void shutdown();
+void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void update();
+
+GLFWwindow *window = nullptr;
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "hello" << std::endl;
+    std::cout << "hello bgfx" << std::endl;
+
+    init();
+
+    unsigned int counter = 0;
+    // while (true)
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        update();
+        // bgfx::frame();
+
+        counter++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+
+        // glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    shutdown();
+    // bgfx::shutdown(); // question : after or before glfwTerminate ?
+    // glfwTerminate();
+
+    return 0;
+}
+
+void update()
+{
+    bgfx::setViewRect(0, 0, 0, uint16_t(WIN_WIDTH), uint16_t(WIN_HEIGHT));
+    bgfx::touch(0);
+
+    bgfx::frame();
+}
+
+void init()
+{
+
     glfwInit();
-    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello, bgfx from glfw!", NULL, NULL);
+    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello, bgfx from glfw!", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+    // glfwMakeContextCurrent(window);
+    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     bgfx::PlatformData pd;
     pd.nwh = glfwGetWin32Window(window);
@@ -33,20 +85,27 @@ int main(int argc, char const *argv[])
 
     bgfx::setDebug(BGFX_DEBUG_TEXT);
 
-
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xFF0000FF, 1.0f, 0);
     bgfx::setViewRect(0, 0, 0, WIN_WIDTH, WIN_HEIGHT);
     bgfx::touch(0);
+}
 
-    unsigned int counter = 0;
-    while (true)
+void shutdown()
+{
+    glfwTerminate();
+    window = nullptr;
+    bgfx::shutdown();
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
-        bgfx::frame();
-        counter++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+        glfwSetWindowShouldClose(window, true);
     }
-    
-    
+}
 
-    return 0;
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    // glViewport(0, 0, width, height);
 }
