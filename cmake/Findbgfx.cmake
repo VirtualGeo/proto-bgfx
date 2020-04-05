1ceb003e685a031d0b8b9cfc41744680e9464833
Charly Mourglia <charly.mourglia@diginext.fr>
2020-04-05 20:26:06 +0200

    Declared a BGFX target
    Links dynamically with debug / release libraries
    Adds MSVC specific declarations
    Adds proper include directories

--- cmake/Findbgfx.cmake
+++ cmake/Findbgfx.cmake
@@ -12,116 +12,132 @@ include(FindPackageHandleStandardArgs)
 
 
 find_path(BGFX_INCLUDE_DIR bgfx/bgfx.h
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        include
-        )
-
+    PATH_SUFFIXES
+    include
+    )
 
 if (UNIX)
-        set(BGFX_INCLUDE_DIRS ${BGFX_INCLUDE_DIR})
+    set(BGFX_INCLUDE_DIRS ${BGFX_INCLUDE_DIR})
 elseif(MSVC)
-        set(BGFX_INCLUDE_DIRS ${BGFX_INCLUDE_DIR}; ${BGFX_INCLUDE_DIR}/compat/msvc)
+    set(BGFX_INCLUDE_DIRS ${BGFX_INCLUDE_DIR} ${BGFX_INCLUDE_DIR}/compat/msvc)
 endif()
 
-
-# message(FATAL_ERROR "ERROR: ${BGFX_INCLUDE_DIR}")
-
-
-
 find_library(BGFX_LIBRARY_DEBUG NAMES bgfxd
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 find_library(BGFX_LIBRARY_RELEASE NAMES bgfx
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 
 find_library(BIMG_LIBRARY_DEBUG NAMES bimgd
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 find_library(BIMG_LIBRARY_RELEASE NAMES bimg
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 
 find_library(BX_LIBRARY_DEBUG NAMES bxd
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 find_library(BX_LIBRARY_RELEASE NAMES bx
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 
 find_library(ASTCCODEC_LIBRARY_DEBUG NAMES astc-codecd
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 find_library(ASTCCODEC_LIBRARY_RELEASE NAMES astc-codec
-        PATHS
-        /usr
-        /usr/local
-        ${BGFX_ROOT}
+    PATHS
+    /usr
+    /usr/local
+    ${BGFX_ROOT}
 
-        PATH_SUFFIXES
-        lib
-        )
+    PATH_SUFFIXES
+    lib
+    )
 
+find_package_handle_standard_args(bgfx
+    REQUIRED_VARS
+        BGFX_INCLUDE_DIR
+        BX_LIBRARY_RELEASE BX_LIBRARY_DEBUG
+        BIMG_LIBRARY_RELEASE BIMG_LIBRARY_DEBUG
+        BGFX_LIBRARY_RELEASE BGFX_LIBRARY_DEBUG
+    FAIL_MESSAGE
+        "Could not find all bgfx libraries. Please check BGFX_ROOT has been properly set, and both debug and release versions of bgfx have been compiled."
+)
 
+mark_as_advanced(BGFX_FOUND
+    BGFX_INCLUDE_DIR
+    BX_LIBRARY_RELEASE BX_LIBRARY_DEBUG
+    BIMG_LIBRARY_RELEASE BIMG_LIBRARY_DEBUG
+    BGFX_LIBRARY_RELEASE BGFX_LIBRARY_DEBUG
+)
 
-# set (BGFX_LIBRARIES_DEBUG "${BX_LIBRARY_DEBUG};${BGFX_LIBRARY_DEBUG}")
-# set (BGFX_LIBRARIES_RELEASE "${BX_LIBRARY_RELEASE};${BGFX_LIBRARY_RELEASE}")
-
-
-find_package_handle_standard_args(bgfx DEFAULT_MSG BGFX_LIBRARY_RELEASE BGFX_INCLUDE_DIR)
-mark_as_advanced(BGFX_FOUND BGFX_INCLUDE_DIR BGFX_LIBRARY_RELEASE)
-#find_package_handle_standard_args(bgfx DEFAULT_MSG BGFX_LIBRARY_DEBUG BGFX_INCLUDE_DIR)
-#mark_as_advanced(BGFX_FOUND BGFX_INCLUDE_DIR BGFX_LIBRARY_DEBUG)
-
-
-# message(FATAL_ERROR "toto: ${BGFX_FOUND}")
-if (NOT BGFX_FOUND) 
-        message(FATAL_ERROR "could not find bgfx, make sure you have set BGFX_ROOT")
+if(BGFX_FOUND)
+    if(NOT TARGET BGFX::BGFX)
+        add_library(BGFX::BGFX INTERFACE IMPORTED)
+        set_property(TARGET BGFX::BGFX PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${BGFX_INCLUDE_DIR} $<$<CXX_COMPILER_ID:MSVC>:${BGFX_INCLUDE_DIR}/compat/msvc>)
+        set_property(TARGET BGFX::BGFX PROPERTY INTERFACE_LINK_LIBRARIES
+            $<$<CONFIG:Debug>:${BGFX_LIBRARY_DEBUG}>
+            $<$<CONFIG:Debug>:${BIMG_LIBRARY_DEBUG}>
+            $<$<CONFIG:Debug>:${BX_LIBRARY_DEBUG}>
+            $<$<CONFIG:Debug>:${ASTCCODEC_LIBRARY_DEBUG}>
+            $<$<CONFIG:Release>:${BGFX_LIBRARY_RELEASE}>
+            $<$<CONFIG:Release>:${BIMG_LIBRARY_RELEASE}>
+            $<$<CONFIG:Release>:${BX_LIBRARY_RELEASE}>
+            $<$<CONFIG:Release>:${ASTCCODEC_LIBRARY_RELEASE}>)
+        set_property(TARGET BGFX::BGFX PROPERTY INTERFACE_COMPILE_DEFINITIONS
+            $<$<CXX_COMPILER_ID:MSVC>:__STDC_LIMIT_MACROS>
+            $<$<CXX_COMPILER_ID:MSVC>:__STDC_FORMAT_MACROS>
+            $<$<CXX_COMPILER_ID:MSVC>:__STDC_CONSTANT_MACROS>)
+    endif()
+else()
+    message(FATAL_ERROR "could not find bgfx, make sure you have set BGFX_ROOT")
 endif()
